1aafdf3c0e7495ee0960c4d43991c7666fe916b8
maxfaber <maxfaber@quicknet.nl>
2022-11-11 00:49:18 +0100

    Changed layer names according to conventions in slides, added gradients of a few layers (with matrix*matrix & matrix*vector)

--- assignment_1/nn.py
+++ assignment_1/nn.py
@@ -12,66 +12,75 @@ def softmax(o_i, O):
 def init_parameters():
     parameters = dict()
 
-    # parameters['w'] = [[1., 1., 1.], [-1., -1., -1.]]
-    # parameters['b'] = [0., 0., 0.]
-    # parameters['k'] = [0., 0., 0.]
-    # parameters['h'] = [0., 0., 0.]
+    # parameters['w1'] = [[1., 1., 1.], [-1., -1., -1.]]
+    # parameters['b1'] = [0., 0., 0.]
+    # parameters['k1'] = [0., 0., 0.]
+    # parameters['h1'] = [0., 0., 0.]
     #
-    # parameters['v'] = [[1., -1., -1.], [1., -1., -1.]]
-    # parameters['c'] = [0., 0.]
-    # parameters['j'] = [0., 0.]
-    # parameters['y'] = [0., 0.]
+    # parameters['w2'] = [[1., -1., -1.], [1., -1., -1.]]
+    # parameters['b2'] = [0., 0.]
+    # parameters['k2'] = [0., 0.]
+    # parameters['h2'] = [0., 0.]
 
-    parameters['w1'] = [[1., 1., 1.], [-1., -1., -1.]]
-    parameters['b1'] = [0., 0., 0.]
-    parameters['k1'] = [0., 0., 0.]
-    parameters['h1'] = [0., 0., 0.]
+    parameters['w'] = [[1., 1., 1.], [-1., -1., -1.]]
+    parameters['b'] = [0., 0., 0.]
+    parameters['k'] = [0., 0., 0.]
+    parameters['h'] = [0., 0., 0.]
 
-    parameters['w2'] = [[1., -1., -1.], [1., -1., -1.]]
-    parameters['b2'] = [0., 0.]
-    parameters['k2'] = [0., 0.]
-    parameters['h2'] = [0., 0.]
+    parameters['v'] = [[1., -1., -1.], [1., -1., -1.]]
+    parameters['c'] = [0., 0.]
+    parameters['o'] = [0., 0.]
+    parameters['y'] = [0., 0.]
     return parameters
 
 
 def forward_pass(x, parameters):
-    for j in range(len(parameters['k1'])):
+    for j in range(len(parameters['k'])):
         for i in range(len(x)):
-            parameters['k1'][j] += parameters['w1'][i][j] * x[i]
-        parameters['k1'][j] += parameters['b1'][j]
-        parameters['h1'][j] = sigmoid(parameters['k1'][j])
+            parameters['k'][j] += parameters['w'][i][j] * x[i]
+        parameters['k'][j] += parameters['b'][j]
+        parameters['h'][j] = sigmoid(parameters['k'][j])
 
-    for i in range(len(parameters['b2'])):
-        for j in range(len(parameters['h1'])):
-            parameters['k2'][i] += parameters['h1'][j] * parameters['w2'][i][j]
-        parameters['k2'][i] += parameters['b2'][i]
-    for i in range(len(parameters['h2'])):
-        parameters['h2'][i] += softmax(parameters['k2'][i], parameters['k2'])
+    for i in range(len(parameters['c'])):
+        for j in range(len(parameters['h'])):
+            parameters['o'][i] += parameters['h'][j] * parameters['v'][i][j]
+        parameters['o'][i] += parameters['c'][i]
+    for i in range(len(parameters['y'])):
+        parameters['y'][i] += softmax(parameters['o'][i], parameters['o'])
     pass
 
 
 def backward_pass(parameters):
     # https://stackoverflow.com/questions/33541930/how-to-implement-the-softmax-derivative-independently-from-any-loss-function/46028029#46028029
     grads = dict()
-    grads['dh2'] = [[], []]
-    grads['dk2'] = []
-    grads['dw2'] = []
 
-    for i in range(len(parameters['h2'])):
-        for j in range(len(parameters['h2'])):
+    grads['dy_o'] = [[], []]
+    range_o = range(len(parameters['o']))
+    for i in range_o:
+        for j in range_o:
             if i == j:
-                dh2 = parameters['h2'][i] * (1 - parameters['h2'][j])
+                dy_o = parameters['o'][i] * (1. - parameters['o'][j])
             else:
-                dh2 = -parameters['h2'][i] * parameters['h2'][j]
-            grads['dh2'][i].append(dh2)
-    for i in range(len(parameters['k2'])):
-        dk2 = 0.
-        # dw2 = 0.
-        for j in range(len(grads['dh2'][i])):
-            dk2 += grads['dh2'][i][j] * parameters['k2'][i]
-            # dw2 += grads['dh2'][i][j] * parameters['w2'][i]
-        grads['dk2'].append(dk2)
-        # grads['dw2'].append(dw2)
+                dy_o = -parameters['o'][i] * parameters['o'][j]
+            grads['dy_o'][i].append(dy_o)
+
+    grads['do_v'] = []
+    grads['do_h'] = []
+    range_dy_o = range(len(grads['dy_o']))
+    range_v = range(len(parameters['v']))
+    range_h = range(len(parameters['h']))
+    for i in range_dy_o:
+        do_v = 0.
+        for j in range_v:
+            do_v += grads['dy_o'][i][j] * parameters['h'][i]
+        grads['do_v'].append(do_v)
+    for i in range_dy_o:
+        d = [0 for _ in range_h]
+        for j in range_v:
+            for k in range_h:
+                d[k] += grads['dy_o'][i][j] * parameters['v'][i][k]
+        grads['do_h'].append(d)
+
     pass
 
 
